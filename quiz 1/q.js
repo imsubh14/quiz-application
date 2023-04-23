7ca59fb4ef0f0102107648abebfdfcde7b92523b
Shubh Gupta <118635326+imsubh14@users.noreply.github.com>
2023-04-23 22:17:11 +0530

    Add files via upload

---
+++ quiz 1/q.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the father of Geometry?",
+    answer: "Euclid",
+    options: [
+      "Pythagoras",
+      "Aristotle",
+      "Euclid",
+      "Kepler"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who was known as Iron man of India?",
+    answer: "Sardar Vallabhbhai Patel",
+    options: [
+      "Govind Ballabh Pant",
+      "Jawaharlal Nehru",
+      "Subhash Chandra Bose",
+      "Sardar Vallabhbhai Patel"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The nucleus of an atom consists of",
+    answer: "protons and neutrons",
+    options: [
+      "electrons and neutrons",
+      "electrons and protons",
+      "protons and neutrons",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Garampani sanctuary is located at",
+    answer: "Diphu, Assam",
+    options: [
+      "Junagarh, Gujarat",
+      "Diphu, Assam",
+      "Kohima, Nagaland",
+      "Gangtok, Sikkim"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The Centre for Cellular and Molecular Biology is situated at",
+    answer:"Hyderabad",
+    options: [
+      "Patna",
+      "Agra",
+      "Jaipur",
+      "Hyderabad"
+    ]
+  }
+];
